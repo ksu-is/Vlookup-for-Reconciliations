96590e31c46643ad409dafa7bdf3ef692c53bb93
charlesdahling <110990015+charlesdahling@users.noreply.github.com>
2022-12-06 12:11:09 -0500

    Update FinalProject.py
    Updated the code to include 2 spreadsheets and merge them together and do the vlookup

--- FinalProject.py
+++ FinalProject.py
@@ -1,4 +1,15 @@
 import pandas as pd
+input("Press Enter when templates are filled out")
 
-excel_report = pd.read_excel("Vlookup sample data.xlsx")
-print(excel_report[excel_report["usrMVS_ID"].str.contains("A{2}")])
+
+smartreport_data = pd.read_excel('smartreport.xlsx')
+tmf_data = pd.read_excel('tmf.xlsx')
+
+
+outer_join = pd.merge(smartreport_data, 
+                     tmf_data,
+                     on ='mvsid', 
+                     how ='outer')
+outer_join
+print(outer_join)
+print('Vlookup Complete if records = NaN Please take the correct action')
