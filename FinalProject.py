097773cd39f677fcd9a19b67e6d68a2d4ef6c148
charlesdahling <110990015+charlesdahling@users.noreply.github.com>
2022-12-07 08:31:04 -0500

    Update FinalProject.py
    I added a way to run the program once the spreadsheets are updated.

--- FinalProject.py
+++ FinalProject.py
@@ -1,15 +1,26 @@
 import pandas as pd
 input("Press Enter when templates are filled out")
+def main():
+    
+    smartreport_data = pd.read_excel('smartreport.xlsx')
+    tmf_data = pd.read_excel('tmf.xlsx')
 
 
-smartreport_data = pd.read_excel('smartreport.xlsx')
-tmf_data = pd.read_excel('tmf.xlsx')
+    outer_join = pd.merge(smartreport_data, 
+                        tmf_data,
+                        on ='mvsid', 
+                        how ='outer')
+    outer_join
+    print(outer_join)
+    print('If records = NaN Please take the correct action and run report again')
 
-
-outer_join = pd.merge(smartreport_data, 
-                     tmf_data,
-                     on ='mvsid', 
-                     how ='outer')
-outer_join
-print(outer_join)
-print('Vlookup Complete if records = NaN Please take the correct action')
+while True:
+    answer = input("Run again once the actions are taken (y/n): ")
+    if answer not in ('y', 'n'):
+        print("Invalid input.")
+        break
+    if answer == 'y':
+        main()
+    else:
+        print("Reconciliation complete!")
+        break
